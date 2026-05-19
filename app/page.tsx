be42d52436613b30b2ace9e4b2833117cbec7679
Kushal Jivan <kjivan525@gmail.com>
2026-05-18 20:48:33 -0400

    feat: assemble full single-page site

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import About from '@/components/About'
 import Services from '@/components/Services'
 import Testimonials from '@/components/Testimonials'
 import BookSession from '@/components/BookSession'
+import ContactSection from '@/components/ContactSection'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -15,7 +17,9 @@ export default function Home() {
         <Services />
         <Testimonials />
         <BookSession />
+        <ContactSection />
       </main>
+      <Footer />
     </>
   )
 }
